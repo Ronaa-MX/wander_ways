ef63853098f8381440c236f40c38e012386a1a6d
Aaron <aaron.ortiz.mendoza.ipn@gmail.com>
2024-04-02 16:51:45 -0600

    requirements

--- src/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-import geopy
-
-app = Flask(__name__)
-@app.route('/')
-
-def map_func():
-    return render_template()
