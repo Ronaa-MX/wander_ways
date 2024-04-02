dbbf3e5fb7be3ef60aca9801f91e71b141317518
Aaron <aaron.ortiz.mendoza.ipn@gmail.com>
2024-04-02 16:49:53 -0600

    requirements

--- back-end/src/app.py
+++ back-end/src/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_marhsmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 import geopy
 
 app = Flask(__name__)
-app.config["SQL_ALCHEMY_DATABASE_URI"]='sqlite://///home/paaron/Documentos/Proyectos/wonder_ways/back-end/src/sites.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite://///home/paaron/Documentos/Proyectos/wonder_ways/back-end/src/sites.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+database = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 @app.route('/',)
 @app.route('/museams')
