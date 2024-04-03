2cd03800b5d17faac1c2aa3cfb86768d6b511fc9
Aaron <aaron.ortiz.mendoza.ipn@gmail.com>
2024-04-03 10:48:16 -0600

    Ultime change

--- back-end/src/app.py
+++ back-end/src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_marhsmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-import geopy
+from sqlalchemy.orm import DeclaritiveBase
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite://///home/paaron/Documentos/Proyectos/wonder_ways/back-end/src/sites.db'
@@ -9,6 +9,22 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 database = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+#User table
+
+#Sites table
+with app.app_context():
+    database.reflect()
+class museams(database.model):
+    __table__= database.metadata.tables["museams"]
+
+#Coordinates of sites table
+
+    
+#Images table
+
+#Progess table
+with
 
 @app.route('/',)
 @app.route('/museams')
